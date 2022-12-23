820be8529fd5d5db99e24c11d7f52e2968b572cb
Hamed Amraoui <hamed.amraoui@etudiant-fst.utm.tn>
2022-12-23 23:27:54 +0100

    Upload Image

--- app/src/main/java/com/example/chattogetherapp/Profile.java
+++ app/src/main/java/com/example/chattogetherapp/Profile.java
@@ -1,8 +1,10 @@
 package com.example.chattogetherapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -11,14 +13,20 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class Profile extends AppCompatActivity {
 
-    private Button btnLogOut;
+    private Button btnLogOut,btnUpload;
     private ImageView ProfileImg;
     private Uri imagePath;
 
@@ -28,7 +36,15 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         btnLogOut = findViewById(R.id.btnLogOut);
+        btnUpload = findViewById(R.id.btnUploadImage);
         ProfileImg = findViewById(R.id.profile_img);
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UploadImage();
+            }
+        });
 
         ProfileImg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,4 +84,23 @@ public class Profile extends AppCompatActivity {
         }
         ProfileImg.setImageBitmap(bitmap);
     }
+
+    private void UploadImage (){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading...");
+        progressDialog.show();
+
+        FirebaseStorage.getInstance().getReference("image/"+ UUID.randomUUID().toString()).putFile(imagePath).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(Profile.this, "Image Uploaded!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(Profile.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+                progressDialog.dismiss();
+            }
+        });
+    }
+
 }
